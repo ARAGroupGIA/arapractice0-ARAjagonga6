d5444229db04a9bd7dc125be22b7339c867d64e0
ARAjagonga6 <55189108+ARAjagonga6@users.noreply.github.com>
2019-09-11 14:22:22 +0200

    Version 1

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,5 +1,12 @@
 /* Here, you should entry the required libraries */
-
+#include <stdio.h>
 void main(){
-	/* Here, you should write the source code of your program */
+int number1;
+int number2;
+printf("We are going to sum 2 numbers, introduce number 1:");
+scanf("%d", &number1);
+printf("introduce number 2:");
+scanf("%d", &number2);
+printf("The result is %d", number1+number2);
+
 }
